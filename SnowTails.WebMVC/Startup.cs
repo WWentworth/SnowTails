bc1ae1837b6a6c6f7cc44f69ddc9e3c02f7c5659
William Wentworth <WWentworth@live.com>
2022-02-07 18:16:34 -0500

    initial setup

---
+++ SnowTails.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SnowTails.WebMVC.Startup))]
+namespace SnowTails.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
